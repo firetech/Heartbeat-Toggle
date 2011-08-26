b54ee2048c4007cedea5c3f2735f5948c48b2173
Joakim Andersson <jocke@barbanet.com>
2011-08-26 21:45:18 +0200

    Refactor app to only require one SU request.

--- src/nu/firetech/android/heartbeat/HeartbeatToggle.java
+++ src/nu/firetech/android/heartbeat/HeartbeatToggle.java
@@ -35,9 +35,9 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class HeartbeatToggle extends Activity {
-	private static final String[] STATUS_CMD = new String[]{"su", "-c", "cat /sys/class/leds/cpu/trigger"};
+	private static final String STATUS_CMD = "cat /sys/class/leds/cpu/trigger";
 	
-	private static final String[] CONTROL_CMD = new String[]{"su", "-c", "cat > /sys/class/leds/cpu/trigger"};
+	private static final String CONTROL_CMD = "echo %s > /sys/class/leds/cpu/trigger";
 	private static final String ON_MODE = "heartbeat";
 	private static final String OFF_MODE = "none";
 
@@ -54,11 +54,11 @@ public class HeartbeatToggle extends Activity {
 	}
 
 	private void toggle() {
-		String status = exec(STATUS_CMD, null);
+		String status = exec(STATUS_CMD);
 		
 		boolean is_on = (status.indexOf("[" + ON_MODE + "]") != -1);
 		
-		exec(CONTROL_CMD, (is_on ? OFF_MODE : ON_MODE));
+		exec(String.format(CONTROL_CMD, (is_on ? OFF_MODE : ON_MODE)));
 		Toast t = Toast.makeText(this, "CPU heartbeat LED " + (is_on ? "disabled" : "enabled"), Toast.LENGTH_LONG);
 		t.show();
 		
@@ -66,19 +66,18 @@ public class HeartbeatToggle extends Activity {
 		this.finish();
 	}
 
-	private String exec(String[] commands, String input) {
+	private String exec(String command) {
 		String s;
 		StringBuilder sb = new StringBuilder();
 		try {
-			Process p = Runtime.getRuntime().exec(commands);
+			Process p = Runtime.getRuntime().exec("su");
 			InputStream stdout = p.getInputStream ();
 			BufferedReader reader = new BufferedReader (new InputStreamReader(stdout));
-			if (input != null) {
-				OutputStream stdin = p.getOutputStream();
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-				writer.write(input);
-				writer.close();
-			}
+			OutputStream stdin = p.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+			writer.write(command);
+			writer.write('\n');
+			writer.close();
 			p.waitFor();
 			while ((s = reader.readLine ()) != null) {
 				sb.append(s);
